3d459dcb838de086ea473e78b9c0af78d605c3f1
sanghyeon <yvyv12345@naver.com>
2025-04-08 15:00:41 +0900

    refactor: RefreshOverlay 적용 위해 루트 layout 수정 및 /customer/layout.tsx 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,27 @@
-import './customer/globals.css';
+import type {Metadata} from "next";
+import "./customer/globals.css";
+import Header from "./customer/components/Header/Header";
+import RefreshOverlay from "./customer/components/RefreshOverlay/RefreshOverlay";
+import type {ReactNode} from "react";
+import SessionWrapper from "./customer/SessionWrapper";
 
-export const metadata = {
-    title: 'My App',
-    description: 'Welcome!',
+export const metadata: Metadata = {
+    title: "카셀렉트",
+    description: "어서오십샤...",
+    icons: {icon: "./customer/favicon.ico"}
 };
 
-export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode;
-}) {
+export default function RootLayout({children,}: { children: React.ReactNode; }) {
     return (
         <html lang="ko">
-        <body>{children}</body>
+        <body>
+        <SessionWrapper>
+            <Header/>
+            <RefreshOverlay>
+                {children}
+            </RefreshOverlay>
+        </SessionWrapper>
+        </body>
         </html>
     );
 }
