2d7fbfa322a5392109471fff0148f05950cd536a
sagakteok <maze77777@naver.com>
2025-05-15 23:06:45 +0900

    Add Header account info style

--- src/app/customer/components/Header/DesktopHeader.tsx
+++ src/app/customer/components/Header/DesktopHeader.tsx
@@ -1,39 +1,80 @@
 'use client';
 
-import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Icon from "@mdi/react";
-import {mdiMagnify, mdiAccountCircle, mdiLogout} from "@mdi/js";
-import { AppBar, Toolbar, IconButton } from "@mui/material";
-import "./Header.scss";
-import {signOut, useSession} from "next-auth/react";
+import { mdiMagnify, mdiAccountCircle, mdiLogout } from "@mdi/js";
+import { AppBar, Toolbar, IconButton, Button } from "@mui/material";
+import styles from "./Header.module.scss";
+import { signOut, useSession } from "next-auth/react";
+import { useEffect, useRef, useState } from "react";
 
 export default function DesktopHeader() {
     const pathname = usePathname();
     const { data: session, status } = useSession();
+    const isLoggedIn = status === "authenticated" && session?.user;
+    const [isAccountActive, setIsAccountActive] = useState(false);
+    const wrapperRef = useRef<HTMLDivElement>(null);
+
+    const toggleAccountBox = () => {
+        setIsAccountActive((prev) => !prev);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if (
+                wrapperRef.current &&
+                !wrapperRef.current.contains(event.target as Node)
+            ) {
+                setIsAccountActive(false);
+            }
+        };
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
     return (
-        <AppBar position="fixed" className="DesktopHeader_AppBarStyle">
-            <div className="DesktopHeader_Container">
-                <Toolbar className="DesktopHeader_ToolbarStyle">
-                    <Link href="/" style={{color: "#FFFFFF", textDecoration: "none"}}>카셀렉트</Link>
-                    <Link href="/customer/estimate" className={`DesktopHeader_text ${pathname === "/estimate" ? "active" : ""}`}>견적 짜기</Link>
-                    <Link href="/Businesses" className={`DesktopHeader_text ${pathname === "/Businesses" ? "active" : ""}`}>나의 견적</Link>
-                    <Link href="/TownCommunity" className={`DesktopHeader_text ${pathname === "/TownCommunity" ? "active" : ""}`}>나의 상담</Link>
-                    <div style={{ marginLeft: "auto" }}>
-                        <IconButton disableTouchRipple component={Link} href="/" className={`DesktopHeader_icon ${pathname === "/" ? "active" : ""}`}><Icon path={mdiMagnify} size={1} /></IconButton>
-                        <IconButton disableTouchRipple component={Link} href="/customer/auth/signin" className={`DesktopHeader_icon ${pathname === "/customer/auth/signin" ? "active" : ""}`}><Icon path={mdiAccountCircle} size={1} /></IconButton>
-                        {/*로그인시, oo님*/}
-                        {status === "authenticated" && session?.user?.name && (
-                            <span style={{ color: "#FFFFFF", fontSize: "0.9rem" }}>
-                                {session.user.name}님
-                            </span>
+        <AppBar position="fixed" className={styles.DesktopHeader_AppBarStyle}>
+            <div className={styles.DesktopHeader_Container}>
+                <Toolbar className={styles.DesktopHeader_ToolbarStyle}>
+                    <Link href="/" className={styles.DesktopHeader_Brand}>카셀렉트</Link>
+                    <Link href="/customer/estimate" className={`${styles.DesktopHeader_text} ${pathname === "/estimate" ? styles.active : ""}`}>견적 짜기</Link>
+                    <Link href="/Businesses" className={`${styles.DesktopHeader_text} ${pathname === "/Businesses" ? styles.active : ""}`}>나의 견적</Link>
+                    <Link href="/TownCommunity" className={`${styles.DesktopHeader_text} ${pathname === "/TownCommunity" ? styles.active : ""}`}>나의 상담</Link>
+
+                    <div ref={wrapperRef} className={styles.DesktopHeader_AccountBoxWrapper}>
+                        <IconButton disableTouchRipple component={Link} href="/" className={`${styles.DesktopHeader_icon} ${pathname === "/" ? styles.active : ""}`}><Icon path={mdiMagnify} size={1} /></IconButton>
+                        {isLoggedIn ? (
+                            <div className={`${styles.DesktopHeader_AccountInfo} ${isAccountActive ? styles.active : ""}`} onClick={toggleAccountBox}>
+                                <Icon path={mdiAccountCircle} size={1} color={isAccountActive ? "#F7D7C5" : "#7A8499"} />
+                                <span className={styles.DesktopHeader_AccountInfoUsername}>{session.user.name}
+                                    <span className={styles.DesktopHeader_AccountInfoCustomerDefault}> 고객님</span>
+                                </span>
+                            </div>
+                        ) : (
+                            <IconButton disableTouchRipple component={Link} href="/customer/auth/signin" className={`${styles.DesktopHeader_icon} ${pathname === "/customer/auth/signin" ? styles.active : ""}`} title="로그인">
+                                <Icon path={mdiAccountCircle} size={1} />
+                            </IconButton>
                         )}
-                        {status === "authenticated" && (
-                            <IconButton disableTouchRipple onClick={() => signOut({callbackUrl: "/customer"})} className= "DesktopHeader_icon" title= "로그아웃"><Icon path={mdiLogout} size={1} /></IconButton>
+
+                        {isLoggedIn && isAccountActive && (
+                            <div className={styles.DesktopHeader_AccountInfoForm}>
+                                <div className={styles.DesktopHeader_AccountNameContainer}>
+                                    <div className={styles.DesktopHeader_AccountName}>{session.user.name}</div>
+                                    <div className={styles.DesktopHeader_AccountRole}>고객</div>
+                                </div>
+                                <div className={styles.DesktopHeader_AccountButtons}>
+                                    <div className={styles.DesktopHeader_AccountManage}>계정 관리</div>
+                                    <Button className={styles.DesktopHeader_LogoutButton} variant="contained" onClick={() => signOut({ callbackUrl: "/customer" })}>
+                                        <Icon path={mdiLogout} size={0.8} color="#FFFFFF" />로그아웃
+                                    </Button>
+                                </div>
+                            </div>
                         )}
-                        </div>
+                    </div>
                 </Toolbar>
             </div>
         </AppBar>
--- src/app/customer/components/Header/MobileHeader.tsx
+++ src/app/customer/components/Header/MobileHeader.tsx
@@ -5,10 +5,10 @@ import { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Icon from "@mdi/react";
-import {mdiMagnify, mdiAccountCircle, mdiMenu, mdiChevronRight, mdiLogout} from "@mdi/js";
+import { mdiMagnify, mdiAccountCircle, mdiMenu, mdiChevronRight, mdiLogout } from "@mdi/js";
 import { AppBar, Toolbar, IconButton, Drawer, List, InputBase, ListItemButton } from "@mui/material";
-import "./Header.scss";
-import {signOut, useSession} from "next-auth/react";
+import styles from "./Header.module.scss";
+import { signOut, useSession } from "next-auth/react";
 
 export default function MobileHeader() {
     const pathname = usePathname();
@@ -18,12 +18,12 @@ export default function MobileHeader() {
 
     return (
         <>
-            <AppBar position="fixed" className="MobileHeader_AppBarStyle">
-                <Toolbar className="MobileHeader_ToolbarStyle">
-                    <Link href="/" style={{color: "#FFFFFF", textDecoration: "none"}}>카셀렉트</Link>
+            <AppBar position="fixed" className={styles.MobileHeader_AppBarStyle}>
+                <Toolbar className={styles.MobileHeader_ToolbarStyle}>
+                    <Link href="/" style={{ color: "#FFFFFF", textDecoration: "none" }}>카셀렉트</Link>
                     {/* 헤더 아이콘 버튼 2개 */}
                     <div>
-                        <IconButton disableTouchRipple component={Link} href="/customer/auth/signin" className={`MobileHeader_icon ${pathname === "/customer/auth/signin" ? "active" : ""}`}>
+                        <IconButton disableTouchRipple component={Link} href="/customer/auth/signin" className={`${styles.MobileHeader_icon} ${pathname === "/customer/auth/signin" ? styles.active : ""}`}>
                             <Icon path={mdiAccountCircle} size={1} />
                         </IconButton>
                         {/*로그인시, oo님*/}
@@ -33,9 +33,9 @@ export default function MobileHeader() {
                             </span>
                         )}
                         {status === "authenticated" && (
-                            <IconButton disableTouchRipple onClick={() => signOut({callbackUrl: "/customer"})} className= "DesktopHeader_icon" title= "로그아웃"><Icon path={mdiLogout} size={1} /></IconButton>
+                            <IconButton disableTouchRipple onClick={() => signOut({ callbackUrl: "/customer" })} className={styles.DesktopHeader_icon} title="로그아웃"><Icon path={mdiLogout} size={1} /></IconButton>
                         )}
-                        <IconButton onClick={() => toggleDrawer(!isDrawerOpen)} className={`MobileHeader_icon ${isDrawerOpen ? "active" : ""}`}>
+                        <IconButton onClick={() => toggleDrawer(!isDrawerOpen)} className={`${styles.MobileHeader_icon} ${isDrawerOpen ? styles.active : ""}`}>
                             <Icon path={mdiMenu} size={1} />
                         </IconButton>
                     </div>
@@ -43,40 +43,40 @@ export default function MobileHeader() {
             </AppBar>
             {/* 서랍 */}
             <Drawer anchor="top" open={isDrawerOpen} onClose={() => toggleDrawer(false)} PaperProps={{ style: { boxShadow: "none", top: "60px" } }} ModalProps={{ keepMounted: true, BackdropProps: { style: { backgroundColor: "transparent" } } }} sx={{ position: "relative", zIndex: "3" }}>
-                <List className="MobileHeader_DrawerListStyle">
+                <List className={styles.MobileHeader_DrawerListStyle}>
                     {/* 검색 창 */}
-                    <div className="MobileHeader_DrawerSearchContainer">
+                    <div className={styles.MobileHeader_DrawerSearchContainer}>
                         <InputBase placeholder="검색어를 입력하세요." inputProps={{ 'aria-label': 'search' }} sx={{ ml: 1, flex: 1, fontSize: "18px", fontFamily: "SpoqaHanSansNeo-Thin", color: "#FFFFFF", "& input::placeholder": { color: "#7A8499", opacity: 1 }, "& input:focus": { color: "#FFFFFF" } }} />
-                        <Icon className="MobileHeader_DrawerSearchIconStyle" path={mdiMagnify} size={1}/>
+                        <Icon className={styles.MobileHeader_DrawerSearchIconStyle} path={mdiMagnify} size={1} />
                     </div>
                     {/* 버튼 1 */}
-                    <ListItemButton className="MobileHeader_ListItemButtonStyle" disableTouchRipple component={Link} href="/AllDocuments" onClick={() => toggleDrawer(false)} selected={pathname === "/AllDocuments"}>
-                        <div className="MobileHeader_ListItemButtonOutterContainer">
-                            <div className="MobileHeader_ListItemButtonInnerContainer">
-                                <p className={`MobileHeader_text ${pathname === "/AllDocuments" ? "active" : ""}`}>견적 짜기</p>
-                                <Icon className="MobileHeader_ListItemButtonChevronIconStyle" path={mdiChevronRight} size={1}/>
+                    <ListItemButton className={styles.MobileHeader_ListItemButtonStyle} disableTouchRipple component={Link} href="/AllDocuments" onClick={() => toggleDrawer(false)} selected={pathname === "/AllDocuments"}>
+                        <div className={styles.MobileHeader_ListItemButtonOutterContainer}>
+                            <div className={styles.MobileHeader_ListItemButtonInnerContainer}>
+                                <p className={`${styles.MobileHeader_text} ${pathname === "/AllDocuments" ? styles.active : ""}`}>견적 짜기</p>
+                                <Icon className={styles.MobileHeader_ListItemButtonChevronIconStyle} path={mdiChevronRight} size={1} />
                             </div>
-                            <div className="MobileHeader_ListItemButtonUnderlineStyle"/>
+                            <div className={styles.MobileHeader_ListItemButtonUnderlineStyle} />
                         </div>
                     </ListItemButton>
                     {/* 버튼 2 */}
-                    <ListItemButton className="MobileHeader_ListItemButtonStyle" disableTouchRipple component={Link} href="/Businesses" onClick={() => toggleDrawer(false)} selected={pathname === "/Businesses"}>
-                        <div className="MobileHeader_ListItemButtonOutterContainer">
-                            <div className="MobileHeader_ListItemButtonInnerContainer">
-                                <p className={`MobileHeader_text ${pathname === "/Businesses" ? "active" : ""}`}>나의 상담</p>
-                                <Icon className="MobileHeader_ListItemButtonChevronIconStyle" path={mdiChevronRight} size={1}/>
+                    <ListItemButton className={styles.MobileHeader_ListItemButtonStyle} disableTouchRipple component={Link} href="/Businesses" onClick={() => toggleDrawer(false)} selected={pathname === "/Businesses"}>
+                        <div className={styles.MobileHeader_ListItemButtonOutterContainer}>
+                            <div className={styles.MobileHeader_ListItemButtonInnerContainer}>
+                                <p className={`${styles.MobileHeader_text} ${pathname === "/Businesses" ? styles.active : ""}`}>나의 상담</p>
+                                <Icon className={styles.MobileHeader_ListItemButtonChevronIconStyle} path={mdiChevronRight} size={1} />
                             </div>
-                            <div className="MobileHeader_ListItemButtonUnderlineStyle"/>
+                            <div className={styles.MobileHeader_ListItemButtonUnderlineStyle} />
                         </div>
                     </ListItemButton>
                     {/* 버튼 3 */}
-                    <ListItemButton className="MobileHeader_ListItemButtonStyle" disableTouchRipple component={Link} href="/TownCommunity" onClick={() => toggleDrawer(false)} selected={pathname === "/TownCommunity"}>
-                        <div className="MobileHeader_ListItemButtonOutterContainer">
-                            <div className="MobileHeader_ListItemButtonInnerContainer">
-                                <p className={`MobileHeader_text ${pathname === "/TownCommunity" ? "active" : ""}`}>나의 견적</p>
-                                <Icon className="MobileHeader_ListItemButtonChevronIconStyle" path={mdiChevronRight} size={1}/>
+                    <ListItemButton className={styles.MobileHeader_ListItemButtonStyle} disableTouchRipple component={Link} href="/TownCommunity" onClick={() => toggleDrawer(false)} selected={pathname === "/TownCommunity"}>
+                        <div className={styles.MobileHeader_ListItemButtonOutterContainer}>
+                            <div className={styles.MobileHeader_ListItemButtonInnerContainer}>
+                                <p className={`${styles.MobileHeader_text} ${pathname === "/TownCommunity" ? styles.active : ""}`}>나의 견적</p>
+                                <Icon className={styles.MobileHeader_ListItemButtonChevronIconStyle} path={mdiChevronRight} size={1} />
                             </div>
-                            <div className="MobileHeader_ListItemButtonUnderlineStyle"/>
+                            <div className={styles.MobileHeader_ListItemButtonUnderlineStyle} />
                         </div>
                     </ListItemButton>
                 </List>
