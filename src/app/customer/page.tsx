537823ff861937b6954edd4d3ab1988dd28904f3
donghyeon <jdh08271@naver.com>
2025-12-03 23:21:06 +0900

    customer/page.tsx 수정

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 import Icon from "@mdi/react";
 import Image from "next/image";
 import Link from "next/link";
-import palisade from "./assets/palisade.png";
-import avante from "./assets/avante.png";
-import grandeul from "./assets/grandeul.png";
-import ioniq5 from "./assets/ioniq5.png";
 import { mdiChevronLeft, mdiChevronRight } from "@mdi/js";
 import styles from "./MainHome.module.scss";
+
+import palisade from "./assets/hyundai/SUV/palisade.png";
+import nexo from "./assets/hyundai/EV/nexo.png";
+import ioniq9 from "./assets/hyundai/EV/ioniq9.png";
+import grandeur from "./assets/hyundai/sedan/grandeur.png";
+import kona from "./assets/hyundai/SUV/kona.png";
 
 const cars = [
   {
@@ -21,40 +23,46 @@ const cars = [
     alt: "팰리세이드",
   },
   {
-    name: "AVANTE",
+    name: "NEXO",
     company: "Hyundai Motors",
-    price: "1,575 만원 부터",
-    image: avante,
-    link: "https://www.hyundai.com/kr/ko/e/vehicles/avante/intro",
-    alt: "아반떼",
+    price: "7,644 만원 부터",
+    image: nexo,
+    link: "https://www.hyundai.com/kr/ko/e/vehicles/the-all-new-nexo/intro",
+    alt: "넥쏘",
+  },
+  {
+    name: "IONIQ 9",
+    company: "Hyundai Motors",
+    price: "6,715 만원 부터",
+    image: ioniq9,
+    link: "https://www.hyundai.com/kr/ko/e/vehicles/ioniq9/intro",
+    alt: "아이오닉 9",
   },
   {
     name: "GRANDEUR",
     company: "Hyundai Motors",
-    price: "3,716 만원 부터",
-    image: grandeul,
-    link: "https://www.hyundai.com/kr/ko/e/vehicles/the-all-new-grandeur/intro",
+    price: "3,798 만원 부터",
+    image: grandeur,
+    link: "https://www.hyundai.com/kr/ko/e/vehicles/grandeur/intro",
     alt: "그랜저",
   },
   {
-    name: "IONIQ 5",
+    name: "KONA",
     company: "Hyundai Motors",
-    price: "5,230 만원 부터",
-    image: ioniq5,
-    link: "https://www.hyundai.com/kr/ko/e/vehicles/ioniq5/intro",
-    alt: "아이오닉5",
+    price: "2,446 만원 부터",
+    image: kona,
+    link: "https://www.hyundai.com/kr/ko/e/vehicles/kona/intro",
+    alt: "코나",
   },
 ];
 
 export default function MainHomeDesktop() {
   const [current, setCurrent] = useState(0);
 
-  // 왼쪽(이전) 페이지
   const handlePrev = () => {
     setCurrent((prev) => (prev === 0 ? cars.length - 1 : prev - 1));
   };
 
-  // 오른쪽(다음) 페이지
   const handleNext = () => {
     setCurrent((prev) => (prev === cars.length - 1 ? 0 : prev + 1));
   };
@@ -65,19 +73,41 @@ export default function MainHomeDesktop() {
       <div className={styles.MainHomeStyle}>
         <div className={styles.MainHomeContainer}>
           <div className={styles.MainHomeTopContent}>
-            <div className={styles.MainHomeFirstTitle}><span>최근 나온 신차</span></div>
-            <div className={styles.MainHomeSecondTitle}><span>{car.name}</span></div>
-            <div className={styles.MainHomeThirdTitle}><span>{car.company}</span></div>
-            <div className={styles.MainHomeForthTitle}><span>{car.price}</span></div>
+            <div className={styles.MainHomeFirstTitle}>
+              <span>최근 나온 신차</span>
+            </div>
+            <div className={styles.MainHomeSecondTitle}>
+              <span>{car.name}</span>
+            </div>
+            <div className={styles.MainHomeThirdTitle}>
+              <span>{car.company}</span>
+            </div>
+            <div className={styles.MainHomeForthTitle}>
+              <span>{car.price}</span>
+            </div>
           </div>
+
           <div className={styles.MainHomeBottomContent}>
-            <Link href={car.link} style={{ textDecoration: "none" }} target="_blank" rel="noopener noreferrer">
+            <Link
+                href={car.link}
+                style={{ textDecoration: "none" }}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
               <div className={styles.MainHomeCarHrefContainer}>
                 <span className={styles.MainHomeCarHrefText}>차량 상세 보기</span>
                 <Icon className={styles.MainHomeCarHrefIcon} path={mdiChevronRight} size={1} />
               </div>
             </Link>
-            <Image className={styles.MainHomeCarImage} src={car.image} alt={car.alt} width={1380} />
+
+            <Image
+                className={styles.MainHomeCarImage}
+                src={car.image}
+                alt={car.alt}
+                width={1380}
+                priority={true}
+            />
+
             <div className={styles.MainHomePaginationContainer}>
               <div
                   className={styles.MainHomePaginationLeftIcon}
@@ -86,7 +116,11 @@ export default function MainHomeDesktop() {
               >
                 <Icon path={mdiChevronLeft} size={1.5} />
               </div>
-              <span className={styles.MainHomePaginationText}>{current + 1} / {cars.length}</span>
+
+              <span className={styles.MainHomePaginationText}>
+              {current + 1} / {cars.length}
+            </span>
+
               <div
                   className={styles.MainHomePaginationRightIcon}
                   onClick={handleNext}
