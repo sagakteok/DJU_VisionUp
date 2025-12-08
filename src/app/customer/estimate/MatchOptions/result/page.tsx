bdb6ec10249caf6282b547ba2be1853d0f9a7d67
sagakteok <maze77777@naver.com>
2025-12-09 00:30:23 +0900

    Add MatchOptions Page in customer estimate

---
+++ src/app/customer/estimate/MatchOptions/result/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React, { useState } from "react";
+import { Card, CardContent, CardActions, IconButton, Collapse, Button } from "@mui/material";
+import { mdiChevronDown, mdiChevronUp } from "@mdi/js";
+import Icon from "@mdi/react";
+import Image from "next/image";
+import styles from "./Estimate.module.scss";
+import { useRouter } from "next/navigation";
+
+import AvanteSide from "../../../assets/hyundai/sedan/avante_side.png"
+
+export default function MatchOptionsResult() {
+
+}
